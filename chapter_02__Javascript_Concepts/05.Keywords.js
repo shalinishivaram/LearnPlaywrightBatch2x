5342fbcaa1e9d03c98a01dba604752a8938e16f8
shalinishivaram <shalinishivaram@users.noreply.github.com>
2026-09-22 21:55:53 +0530

    Add keywords exercise

---
+++ chapter_02__Javascript_Concepts/05.Keywords.js
@@ -0,0 +1,6 @@
+// Keyowrds are reserved words in JavaScript that have special meaning and cannot be used as identifiers (like variable names, function names, etc.). Here are some examples of keywords in JavaScript:
+/*Declaration: var, let, const, function, class, extends, super
+Values/Types: typeof, instanceof, in, of, new, delete, void, null, undefined, true, false
+Logic/Control: if, else, switch, case, default, for, while, do, break, continue, return, throw, try, catch, finally
+Modules: import, export, from, as
+Others: this, async, await, yield, static, get, set, debugger, with (deprecated), enum (reserved)*/
